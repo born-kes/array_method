31cefa9e4f16f2000e0e6c70e49a6033f619b73d
born-kes <born-kes@o2.pl>
2024-02-12 15:16:22 +0100

    style

--- demo.js
+++ demo.js
@@ -16,61 +16,57 @@ console.log(`---
     const arry = `, icons, `;
 ---
 \`\`\`
-    '${str}'.split(''); => `, str.split(''), //.split [  '\ud83d', '\ude0a', '\ud83d', '\ude06', '\ud83d', '\ude02', '\ud83d', '\ude0d', '\ud83d', '\ude07', '\ud83d', '\ude0e' ]
-    `
+    '${str}'.split(''); => `, str.split(''), `
 \`\`\`
 ---
     Array.from('${str}'); =>`, Array.from(str), `
     
-    Array(3).fill('😍');`, Array(3).fill('😍'));	//  [ '😍', '😍', '😍' ]
+    Array(3).fill('😍');`, Array(3).fill('😍'));
 
-print('fill', '❤');			// [ '❤', '❤', '❤', '❤', '❤' ] => [ '❤', '❤', '❤', '❤', '❤' ]
-print('fill', '❤', 1, 3);		// [ '😆', '❤', '❤', '😇', '😎' ] => [ '😆', '❤', '❤', '😇', '😎' ]
-print('push', '❤');		//  [ '😆', '😂', '😍', '😇', '😎', '❤' ] => 6
-print('unshift', '❤');		//  [ '❤', '😆', '😂', '😍', '😇', '😎' ] => 6
-print('pop');		//  [ '😆', '😂', '😍', '😇' ] =>  😎
-print('shift');		//  [ '😂', '😍', '😇', '😎' ] => '😆'
-print('at', -2);		// [ '😆', '😂', '😍', '😇', '😎' ] =>  😇
-print('slice', -4, 3); // [ '😆', '😂', '😍', '😇', '😎' ] =>  [ '😂', '😍' ]
-print('splice', 1, 2); // [ '😆', '😇', '😎' ] => ['😂', '😍' ]
-print('splice', 1, 2, '😆');  //  [ '😆', '😆', '😇', '😎' ] =>  [ '😂', '😍' ]
-print('splice', 1, 2, '❤', '👍', '👌', '👋');  //  ['😆', '❤',  '👍', '👌', '👋', '😇', '😎'] => [ '😂', '😍' ]
-print('reverse'); //  [ '😎', '😇', '😍', '😂', '😆' ] =>  [ '😎', '😇', '😍', '😂', '😆' ]
-print('sort');  // [ '😂', '😆', '😇', '😍', '😎' ] => [ '😂', '😆', '😇', '😍', '😎' ]
-
-console.log('===');
-
-print('join', '❤'); //  =>  😆❤😂❤😍❤😇❤😎
-print('concat', '❤', '👍', '👌', '👋'); // => ['😆', '😂', '😍','😇', '😎', '❤','👍', '👌', '👋']
-print('includes', '😍'); // => true
-print('includes', '❤'); // => true
-print('indexOf', '😍'); // => 2
-print('map', (el, i) => `${i}:${el}`); // =>  [ '0:😆', '1:😂', '2:😍', '3:😇', '4:😎' ]
-print('filter', (el, i) => (el === '😍' || i === 4)); // =>  [ '0:😆', '1:😂', '2:😍', '3:😇', '4:😎' ]
-print('reduce', (acc, el) => acc + el, ''); // =>  [ '0:😆', '1:😂', '2:😍', '3:😇', '4:😎' ]
-print('every', (el) => el === '😍'); // =>  false
-print('every', (el) => typeof el === "string"); // =>  true
-print('some', (el) => el === '😍'); // =>  true
+print('fill', '❤');
+print('fill', '❤', 1, 3);
+print('push', '❤');
+print('unshift', '❤');
+print('pop');
+print('shift');
+print('at', -2);
+print('slice', -4, 3);
+print('splice', 1, 2);
+print('splice', 1, 2, '😆');
+print('splice', 1, 2, '❤', '👍', '👌', '👋');
+print('reverse');
+print('sort');
 
 console.log('===');
 
-print('find', (el) => el === '😍'); // => 😍
-print('findIndex', (el) => el === '😍'); // => 2
+print('join', '❤');
+print('concat', '❤', '👍', '👌', '👋');
+print('includes', '😍');
+print('includes', '❤');
+print('indexOf', '😍');
+print('map', (el, i) => `${i}:${el}`);
+print('filter', (el, i) => (el === '😍' || i === 4));
+print('reduce', (acc, el) => acc + el, '');
+print('every', (el) => el === '😍');
+print('every', (el) => typeof el === "string");
+print('some', (el) => el === '😍');
+print('find', (el) => el === '😍');
+print('findIndex', (el) => el === '😍');
 
-console.log("==="); //new line
+console.log("===");
 print('reduceRight', (acc, el) => acc + el, '');
 
 const arrayIconFlat = [['😆', 1, '😆'], [2, '😎', '😎'], [5, '😆']];
 console.log(`---
     const arry =`, arrayIconFlat, `
     arry.flat() =>`, arrayIconFlat.flat()
-); // [ [ '😆', 1, '😆' ], [ 2, '😎', '😎' ], [ 5, '😆' ] ] .flat()  => ['😆', 1, '😆', 2, '😎', '😎', 5, '😆' ]
+);
 
 console.log(`
     const arry =`, arrayIconFlat, `;
     arry.flatMap((el) => el.length>2?el:[]); => `, arrayIconFlat.flatMap((el) => el.length > 2 ? el : []));
 print('reduceRight', (acc, el) => acc + el, '');
-print('copyWithin', 0, 4); // [ '😍', '😆', '😂', '😍', '😇', '😎' ] => [ '😍', '😆', '😂', '😍', '😇', '😎' ]
+print('copyWithin', 0, 4);
 
 print('entries');
 print('values');
@@ -81,4 +77,3 @@ console.log(`---
 for (const [index, icon] of [...icons].entries()) {
     console.log(`    index: ${index}, icon: ${icon}`);
 }
-//*/
